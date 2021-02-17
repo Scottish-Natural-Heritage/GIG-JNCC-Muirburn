0ec234e10eccaf5e4c630def401a00c192d4ae2a
duncansnh <59926353+duncansnh@users.noreply.github.com>
2021-02-17 15:42:14 +0000

    Update operationalcode.py

--- operationalcode.py
+++ operationalcode.py
@@ -118,7 +118,7 @@ def getdatalist(wd, proc_list, grantoproc, months_out):
     inputfiles = []
 
     for r, d, f in os.walk(wd, followlinks=True):
-        for name in glob.fnmatch.filter(f, '*vmsk_sharp_rad_srefdem_stdsref.tif'):
+        for name in glob.fnmatch.filter(f, '*osgb_vmsk_sharp_rad_srefdem_stdsref.tif'):
             # create [imagename, imagepath, granule, size, date]
             size = (os.stat(os.path.join(r, name)).st_size)/(1024*1024*1024)
             if name.split('_')[1][4:6] not in months_out:
