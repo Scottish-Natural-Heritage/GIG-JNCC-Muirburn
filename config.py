da29f1d550a7f43ded27530b56f670343bb54326
duncansnh <59926353+duncansnh@users.noreply.github.com>
2021-10-01 10:19:17 +0100

    MONTHS_LIST parameter

--- config.py
+++ config.py
@@ -8,36 +8,38 @@ Contributors:
     Alastair Graham, Geoger Ltd, @ajggeoger
 """
 # --Main set up--
-ARD_WRKDIR = '/neodc/sentinel_ard/data/sentinel_2/2020'           # Input ARD data, enter a year and use month filters below  
+ARD_WRKDIR = '/neodc/sentinel_ard/data/sentinel_2'           # Input ARD data  
 
 # OPERATIONAL LOCATION
-GWS_DATA = '/gws/nopw/j04/jncc_muirburn/users/abdb2/Phase2_April20' # GWS output location 
+GWS_DATA = '/gws/nopw/j04/jncc_muirburn/users/abdb2/Phase2_2019_2020' # GWS output location (needs an output folder added) 
 
 #TEST LOCATION for when used as a notebook
 #GWS_DATA = '/home/users/abdb2/phase2_test'
 
-# Name for output file for the run - date range and cloud cover thresholds used
-OUT_SHAPE = 'Wildfire_and_muirburn_202004_06_03.shp'
+# Name for output file for the run
+OUT_SHAPE = 'Wildfire_and_muirburn_201905_202004_06_03.shp'
 
 LANDMASK = '/gws/nopw/j04/jncc_muirburn/data/Scot_LandMask/muirburn_mask_phase2.tif' # Landmask shapefile location
+
+#TEST LOCATION for when used as a notebook
+#LANDMASK = '/home/users/abdb2/phase2_test/muirburn_mask_phase2.tif'
 
 # --Other parameters--
 # Image thresholding values - the variable names are set in the code, but the values can be changed here.
 # The type is a helper variable so that users know how it is being applied. It is not used in the code (global == to all images). 
-THRESHOLD = {'threshdsavi': 0.2853, 'threshpostnbr': 0.2395, 'threshdnbr2': 0.8, 'threshdcsi': 0.0, 'type': 'global'}
+THRESHOLD = {'threshdsavi': 0.2853, 'threshpostnbr': 0.2395, 'threshdnbr2': 0.8, 'threshdnbr2_shad': 0.045, 'type': 'global'}
 
 # Image thresholding values - the variable names are set in the code, but the values can be changed here.
 # The type is a helper variable so that users know how it is being applied. It is not used in the code (global == to all images). 
-GROW = {'dsaviq1thresh': 0.206748, 'postnbrq1thresh': 0.173447, 'cloudthresh': 0.8, 'shadowthresh': 0.0,'type': 'global'}
+GROW = {'dsaviq1thresh': 0.206748, 'postnbrq1thresh': 0.173447, 'cloudthresh': 0.8, 'shadowthresh': 0.045,'type': 'global'}
 
 # Cloud cover threshold
 CLOUD = 0.6
-
 # set CLOUD2 to 0.0 if two runs are not required
 CLOUD2 = 0.3
 
 # Scottish granule filter - only process the granules for Scotland. 
-
+#PROC_GRANULES = ['T29UPB']
 PROC_GRANULES = ['T29UPB', 'T29VND', 'T29VNE', 'T29VPC', 'T29VPD', 'T29VPE', 'T30UUF', 'T30UUG', 'T30UVF', 'T30UVG', 'T30UWG', 'T30VUH', 'T30VUJ', 'T30VUK', 'T30VUL', 'T30VVH', 'T30VVJ', 'T30VVK', 'T30VVL', 'T30VWH', 'T30VWJ', 'T30VWL', 'T30VWM', 'T30VWN', 'T30VXM', 'T30VXN']
 
 # Dictionary containing the orbit of maximum coverage for each granule.  NB in three instances there are two orbits.
@@ -71,5 +73,6 @@ MAXORBIT = {'T29UPB':['ORB123'],
               'T30VXN':['ORB080'], 
                }
 
-# Date filter for seasonality - IMPORTANT - ENSURE ONE PREVIOUS MONTH IS INCLUDED AS WELL AS THE DATE RANGE OF INTEREST
-MONTHS_OUT = ['01','02','05','06','07','08','09', '10', '11', '12']
+# Date filter - include month previous to date range required
+MONTHS_LIST = ['/2019/04','/2019/05','/2019/06','/2019/07','/2019/08',
+               '/2019/09','/2019/10','/2019/11','/2019/12','/2020/01','/2020/02','/2020/03','/2020/04']
