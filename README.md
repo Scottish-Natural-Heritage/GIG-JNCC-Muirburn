2173aa906418bb26b1761aa5b483437cd2160da6
duncansnh <59926353+duncansnh@users.noreply.github.com>
2022-04-15 15:47:17 +0100

    Update operationalcode.py (#1)
    * Update operationalcode.py
    
    * Update config.py
    
    * Update operationalcode.py
    
    Phase 2 changes:
    - carry out thresholding on cloud cover metadata
    - test partial granules
    
    * Update config.py
    
    config file includes maximum orbit info for each granule
    
    * Update operationalcode.py
    
    * Delete developmentFiles directory
    
    * Delete oldfiles directory
    
    * S2 ARD corrupt edge pixel fix implemented
    
    * Output filename added
    
    * Update operationalcode.py
    
    Code amended to save shapes to one shapefile instead of multiple
    
    * Update operationalcode.py
    
    new fix for S2 edge effect by converting to zeros
    
    * Logging functionality changed to info level
    
    * major change - cloud cover
    
    Cloud cover is now calculated for each granule against the mask.
    Ability to do two runs at once with different cloud cover thresholds.
    An extra month of prefire  imagery is considered to allow images early in the run to be compared with previous ones.
    
    * Major change - 2 cloud thresholds
    
    * NBR2 threshold and multiyear
    
    Config file now needs a list of year/month combinations to enable processing run between years
    
    * MONTHS_LIST parameter
    
    * bugfix - last month
    
    last month loop break updated to be last month and year to be able to run multiyear
    
    * Thresholds tweaked
    
    dSAVI and postNBR thresholds changed to 0.3 for identifying seed pixels
    
    * Changes to comments
    
    * changes to comments
    
    * Updated for Phase 2 of project
    
    * Update README.md

--- README.md
+++ README.md
@@ -1,41 +1,18 @@
-# JNCCBurnCalcs
-Code linked to the JNCC Muirburn project, designed to run on JASMIN using ARD from CEDA.
+# GIG-JNCC-Muirburn
+Code linked to the NatureScot/JNCC wildfire and muirburn monitoring project, designed to run on JASMIN using analysis ready Sentinel-2 data from the CEDA archive.
 
 ## Summary
-This code is designed to run on a JASMIN Science Server (provided by the JNCC for this particular project). It is an initial attempt to scale up the use of Sentinel 2 (S2) Analysis Ready Data (ARD) - created by the JNCC and hosted on CEDA - for the purposes of mapping wildfire and muirburn areas in Scotland. The methodology for the assessment ofburn detection was provided by staff at Nature Scot and worked into the code in this repository.
+This code is designed to run on a JASMIN Science Server (provided by JNCC for this particular project). It is an attempt to scale up the use of Sentinel-2 (S2) Analysis Ready Data (ARD) - created by the JNCC and hosted on CEDA - for the purposes of mapping wildfire and muirburn areas in Scotland. The methodology for the assessment of burn detection was developed initially by NatureScot EO analysts using trial sites in the Cairngorms and the Isle of Skye with some aspects amended when the code was run nationally.
 
-There is an option to crawl a directory on CEDA and return the number of image granules in each sub-folder to be processed. If not required this can be commented out.
-
-The code crawls a supplied directory and creates a list of valid tif files to be processed. It then filters the list against a list of previously processed images (imported from a pickle file, also available in raw text for ease of reading). This results in a list of new images to be processed, which is sorted by granule ID and date.
-
-The code then creates image pairs for each granule - pre and post potential-burn images. These are used to create dnbr2, dSAVI and postnbr images which are then used in a thresholing process to create seed areas of possible burns.
-
-Outputs are created using the following naming format:
-
-* [IMG]	S2B20200627T31UCVORB094_S2B20200630T31UCVORB137_burnseed.tif	~2 MB	 
-* [IMG]	S2B20200627T31UCVORB094_S2B20200630T31UCVORB137_dnbr2.tif	 	~275 MB	 
-* [IMG]	S2B20200627T31UCVORB094_S2B20200630T31UCVORB137_dsavi.tif		~350 MB	 
-* [IMG]	S2B20200627T31UCVORB094_S2B20200630T31UCVORB137_postnbr.tif	    ~300 MB
-
-As can be seen the seed files are much smaller than the intermediate files. In an operational system the intermediate files should not be saved to disk. The output filenames take the form: 
-
-** preburn image details _ postburn image details _ dataset type .tif **
-
-where:
-S2B = Sentinel satellite
-20200627 = date in yyyymmdd format
-T31UCV = granule ID
-ORB094 = orbit ID
-
-A shapefile is also output.
-
+The code runs through the following steps:
+* Obtains a list of granules to process that match a Scottish granule name and fall within a date range and cloud coverage criteria given in the config.py file.  Note cloud coverage percentages are refined first by combining the cloud mask with a land cover mask giving areas of interest (i.e. area of Scotland excluding sea, water bodies and agricultural areas)
+* Obtains a second list of granules if a second cloud cover threshold has been defined  (this increases the probability of burn detection though takes longer to process)
+* Loops through each granule name in turn (i.e. 'T29UPB' to 'T30VXN') and creates a sublist of granules to process.
+* Identifies an image pair to process (termed pre and post fire image) and masks out areas of water ,agriculture, sea, cloud and cloud shadow and topographic shadow on each.
+* Calculates indices on remaining areas of the granules (NBR, NBR2, SAVI) and the differences in NBR2 and SAVI.
+* Thresholds the index layers using the values in the config.py file to identify core or seed burn pixels.
+* 'Grows' these burn areas using a second set of threshold values.
+* Exports possible burn areas to a shapefile
 
 ## How To
 The code is presented as a single script (operationalcode.py) and configuration file (config.py). To run the code, log into the JASMIN Science Server and clone this repository. Navigate into the repository folder, change the details in the configuration file to match the system set up and run 'python operationalcode.py'.
-
-
-## ToDo
-* Investigate and add more robust error capturing to the code
-* Investigate the use of parallel processing (either on the science server using tools such as dask or on the SLURM cluster batch compute on JASMIN)
-* Improve thresholding values
-* Refactor code into multiple files for ease of reading and debugging
--- config.py
+++ config.py
@@ -1,34 +1,78 @@
 """
+SCOTTISH WILDFIRE AND MUIRBURN MAPPING PROJECT: PHASE 2
 This module contains configuration information for use in the code for running on JASMIN to calculate 
 burn locations in Scotland, using data held on CEDA
     
 Contributors: 
-    Alastair Graham, Geoger Ltd, @ajggeoger
     Duncan Blake, NatureScot
+    Alastair Graham, Geoger Ltd, @ajggeoger
 """
 # --Main set up--
-ARD_WRKDIR = '/neodc/sentinel_ard/data/sentinel_2/2020/04'           # Input ARD data (default is April 2019)  
-GWS = '/gws/nopw/j04/jncc_muirburn'                                  # Group workspace
-GWS_DATA = '/gws/nopw/j04/jncc_muirburn/users/abdb2/duncan_v2'                       # GWS output location (needs an output folder added) 
-LANDMASK = '/gws/nopw/j04/jncc_muirburn/data/Scot_LandMask/landmask_water_v3.tif' # Landmask raster location
+ARD_WRKDIR = '/neodc/sentinel_ard/data/sentinel_2'           # Input ARD data  
+
+# OPERATIONAL LOCATION
+GWS_DATA = '/gws/nopw/j04/jncc_muirburn/users/abdb2/Phase2_2019_2020' # GWS output location (needs an output folder added) 
+
+#TEST LOCATION for when used as a notebook
+#GWS_DATA = '/home/users/abdb2/phase2_test'
+
+# Name for output file for the run - contains start and end dates of run and one or two clod cover thresholds used
+OUT_SHAPE = 'Wildfire_and_muirburn_201905_202004_06_03.shp'
+
+LANDMASK = '/gws/nopw/j04/jncc_muirburn/data/Scot_LandMask/muirburn_mask_phase2.tif' # Masking shapefile location
+
+#TEST LOCATION for when used as a notebook
+#LANDMASK = '/home/users/abdb2/phase2_test/muirburn_mask_phase2.tif'
 
 # --Other parameters--
-# Image thresholding values - the variable names are set in the code, but the values can be changed here.
+# Image thresholding values for core burn pixels - the variable names are set in the code, but the values can be changed here.
 # The type is a helper variable so that users know how it is being applied. It is not used in the code (global == to all images). 
-THRESHOLD = {'threshdsavi': 0.2853, 'threshpostnbr': 0.2395, 'threshdnbr2': 0.8, 'type': 'global'}
+#THRESHOLD = {'threshdsavi': 0.2853, 'threshpostnbr': 0.2395, 'threshdnbr2': 0.8, 'threshdnbr2_shad': 0.045, 'type': 'global'}
+THRESHOLD = {'threshdsavi': 0.3, 'threshpostnbr': 0.3, 'threshdnbr2': 0.8, 'threshdnbr2_shad': 0.045, 'type': 'global'}
 
-# Image thresholding values - the variable names are set in the code, but the values can be changed here.
+# Threshold values for 'growing' the burn areas - the variable names are set in the code, but the values can be changed here.
 # The type is a helper variable so that users know how it is being applied. It is not used in the code (global == to all images). 
-GROW = {'dsaviq1thresh': 0.206748, 'postnbrq1thresh': 0.173447, 'cloudthresh': 0.8, 'type': 'global'}
+GROW = {'dsaviq1thresh': 0.206748, 'postnbrq1thresh': 0.173447, 'cloudthresh': 0.8, 'shadowthresh': 0.045,'type': 'global'}
 
-# Toggle the file count function on and off. Value can be 'off' or 'on'
-FILECOUNT = 'on'
-
-# Cloud cover threshold (for use in future versions, not called in current code)
-#CLOUD = 0.9 
+# Cloud cover threshold
+CLOUD = 0.6
+# set CLOUD2 to 0.0 if two runs are not required
+CLOUD2 = 0.3
 
 # Scottish granule filter - only process the granules for Scotland. 
 PROC_GRANULES = ['T29UPB', 'T29VND', 'T29VNE', 'T29VPC', 'T29VPD', 'T29VPE', 'T30UUF', 'T30UUG', 'T30UVF', 'T30UVG', 'T30UWG', 'T30VUH', 'T30VUJ', 'T30VUK', 'T30VUL', 'T30VVH', 'T30VVJ', 'T30VVK', 'T30VVL', 'T30VWH', 'T30VWJ', 'T30VWL', 'T30VWM', 'T30VWN', 'T30VXM', 'T30VXN']
 
-# Date filter for seasonality - do not process 01 Sept - 31 dec inclusive - Scottish ARD only starts in Feb 2019
-MONTHS_OUT = ['09', '10', '11', '12']
+# Dictionary containing the orbit of maximum coverage for each granule.  NB in three instances there are two orbits.
+# This cannot account for the odd SPLIT granule.
+
+MAXORBIT = {'T29UPB':['ORB123'],
+              'T29VND':['ORB023'],
+              'T29VNE':['ORB023'],
+              'T29VPC':['ORB123'],
+              'T29VPD':['ORB123'],
+              'T29VPE':['ORB023'],
+              'T30UUF':['ORB080'],
+              'T30UUG':['ORB123'],
+              'T30UVF':['ORB080'],
+              'T30UVG':['ORB080'],
+              'T30UWG':['ORB037'],
+              'T30VUH':['ORB123'],
+              'T30VUJ':['ORB123'],
+              'T30VUK':['ORB123'],
+              'T30VUL':['ORB023', 'ORB123'],
+              'T30VVH':['ORB080'],
+              'T30VVJ':['ORB080'],
+              'T30VVK':['ORB123'],
+              'T30VVL':['ORB123'],
+              'T30VWH':['ORB037', 'ORB080'],
+              'T30VWJ':['ORB080'],
+              'T30VWL':['ORB080'],
+              'T30VWM':['ORB080'],
+              'T30VWN':['ORB080','ORB123'],
+              'T30VXM':['ORB080'],
+              'T30VXN':['ORB080'], 
+               }
+
+# Date filter - include month previous to date range required
+MONTHS_LIST = ['/2019/04','/2019/05','/2019/06','/2019/07','/2019/08',
+               '/2019/09','/2019/10','/2019/11','/2019/12','/2020/01','/2020/02','/2020/03','/2020/04']
